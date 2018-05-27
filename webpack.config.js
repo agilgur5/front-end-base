bf68729c67f5dffee49926b25d62bbb5db0fc69c
Anton Gilgur <agilgur5@gmail.com>
2018-05-27 01:15:34 -0700

    (format): format webpack config better

--- webpack.config.js
+++ webpack.config.js
@@ -15,17 +15,35 @@ module.exports = {
   module: {
     rules: [
       // use ! to chain loaders; note that the first loader is rightmost (RtL)
-      { test: /\.es6$/, loader: 'babel-loader', query: {
-        presets: ['es2015', 'react', 'stage-1'],
-        plugins: ['transform-decorators-legacy']
-      }},
-      { test: /\.pcss$/, loader: 'style-loader!css-loader!postcss-loader' },
-      { test: /\.pcssm$/, loader:
-        'style-loader!css-loader?modules&importLoaders=1!postcss-loader' },
-      { test: /\.css$/, loader: 'style-loader!css-loader' },
-      { test: /\.cssm$/, loader: 'style-loader!css-loader?modules' },
+      {
+        test: /\.es6$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react', 'stage-1'],
+          plugins: ['transform-decorators-legacy']
+        }
+      },
+      {
+        test: /\.pcss$/,
+        loader: 'style-loader!css-loader!postcss-loader'
+      },
+      {
+        test: /\.pcssm$/,
+        loader: 'style-loader!css-loader?modules&importLoaders=1!postcss-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      {
+        test: /\.cssm$/,
+        loader: 'style-loader!css-loader?modules'
+      },
       // inline base64 URLs for <=8k images, direct URLs for the rest
-      { test: /\.(png|jpg|svg|gif)$/, loader: 'url-loader?limit=8192' }
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        loader: 'url-loader?limit=8192'
+      }
     ]
   },
   mode: process.env.NODE_ENV
