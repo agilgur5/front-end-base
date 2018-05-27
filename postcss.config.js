47d2aa7d5b32e11cdbee295341ecd14d36ecdacf
Anton Gilgur <agilgur5@gmail.com>
2018-05-27 03:45:02 -0700

    (fix): fix flexbox cross-browser bugs (taken from CRA)
    - some interesting fixes found in create-react-app

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   plugins: [
     require('rucksack-css'),
-    require('postcss-preset-env')
+    require('postcss-flexbugs-fixes'),
+    require('postcss-preset-env')({
+      autoprefixer: { flexbox: 'no-2009' }
+    })
   ]
 }
