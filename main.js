aba6503e2537eb86ef1b4d8030abf854b6cd8481
Anton Gilgur <agilgur5@gmail.com>
2018-08-09 22:14:04 -0700

    (feat): add index.html, main.js, app.js for testing
    - and variations of these are likely to be in most apps
    
    - split main.js and app.js for potential use of react-hot-loader
      in the future
      - it's turned off by default in some boilerplates I've seen due to
        it sometimes causing unexpected bugs
    
    - TODO: replace index.html with a html-webpack-plugin template
      - will need to figure out at some point how to get that optimally
        interact with various sorts of back-end templates

---
+++ main.js
@@ -0,0 +1,9 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './components/app.js'
+
+const element = document.createElement('div')
+document.body.appendChild(element)
+ReactDOM.render(<App />, element)
