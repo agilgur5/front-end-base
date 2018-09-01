0093b8e7ab7b097f9c97be53fe7d48e5f6569805
Anton Gilgur <agilgur5@gmail.com>
2018-08-31 23:30:56 -0700

    (optim): use PureComponent in sample app.js file
    - This is to build better habits
    - I'm so used to writing Component that I forget to use PureComponent
      even though the vast majority of components I write are pure and
      have immutable state

--- components/app.js
+++ components/app.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 // potentially include React Hot Loader here
-export default class App extends React.Component {
+export default class App extends React.PureComponent {
   render = () => {
     return <div>Hello World!</div>
   }
