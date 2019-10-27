cb0c18a6f2358459267ea80d5b6c1a72ccb009e9
Anton Gilgur <agilgur5@gmail.com>
2019-10-27 15:06:34 -0400

    (deps): use cross-fetch instead of isomorphic-fetch
    - Per the cross-fetch FAQ (https://github.com/lquixada/cross-fetch#why-not-isomorphic-fetch),
      isomorphic-fetch has issues with RN, hasn't been maintained in years,
      and has outdated dependencies
    
    - fbjs still uses it unfortunately (and has its own internal fix for
      RN usage), so it's still in the dependency tree :/

--- config/polyfills.js
+++ config/polyfills.js
@@ -4,5 +4,5 @@
   Some features are not implemented by core-js and as such should be
   added to this file, which is a common entry point in the Webpack config.
 */
-import 'core-js/features/promise' // isomorphic-fetch needs a Promise polyfill
-import 'isomorphic-fetch' // fetch is not implemented by core-js
+import 'core-js/features/promise' // cross-fetch needs a Promise polyfill
+import 'cross-fetch/polyfill' // fetch is not implemented by core-js
