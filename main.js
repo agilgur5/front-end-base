bd33321106178b036f7edd1ab7688295dd7cffe7
Anton Gilgur <agilgur5@gmail.com>
2019-12-03 20:09:12 -0500

    even more minimal repro for webpack-plugin-serve ramdisk issues
    - no loaders what-so-ever, plus simplified mode, entry, and output
    - and simplified main.js to literally just a one-line print statement

--- main.js
+++ main.js
@@ -1,8 +1,1 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-import App from './components/app.js'
-
-const element = document.createElement('div')
-document.body.appendChild(element)
-ReactDOM.render(<App />, element)
+console.log('print me pl0x')
--- webpack.config.js
+++ webpack.config.js
@@ -2,43 +2,23 @@ const webpack = require('webpack')
 const path = require('path')
 const { WebpackPluginServe } = require('webpack-plugin-serve')
 
-const isProd = process.env.NODE_ENV === 'production'
-const commonEntry = []
-// https://github.com/shellscape/webpack-plugin-serve/blob/master/recipes/entry-points.md
-// don't include the serve client in production
-if (!isProd) { commonEntry.push('webpack-plugin-serve/client') }
 const outputDir = 'build/'
 
 
 // webpack's configuration
 module.exports = {
-  // setting NODE_ENV does _not_ automatically set mode
-  mode: isProd ? 'production' : 'development',
+  mode: 'development',
   entry: {
-    main: commonEntry.concat('./main.js')
+    main: ['webpack-plugin-serve/client', './main.js']
   },
   output: {
     path: path.join(__dirname, outputDir), // where builds go
-    // `hash` is the only option available in development
-    filename: '[name].bundle.[' + (isProd ? 'contenthash' : 'hash') + '].js'
-  },
-  module: {
-    rules: [
-      // use ! to chain loaders; note that the first loader is rightmost (RtL)
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: { cacheDirectory: !isProd } // only cache in development
-      },
-    ]
   },
   plugins: [
-    // don't include serve in production
-    ...(isProd ? [] : [new WebpackPluginServe({
+    new WebpackPluginServe({
       // currently throwing an error: https://github.com/shellscape/webpack-plugin-serve/issues/152
       ramdisk: true, // development build perf optimization
       log: { level: 'debug' }
-    })])
+    })
   ],
 }
